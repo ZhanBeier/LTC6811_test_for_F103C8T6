757b9113718916453ccd82c4b3e017b65f4e5695
ZZZBear <2411582954@qq.com>
2026-04-15 16:04:14 +0800

    Initial commit

---
+++ Core/Ex/Src/Delay.c
@@ -0,0 +1,17 @@
+//
+// Created by ZZZBear on 2026/4/15.
+//
+
+#include "delay.h"
+
+void delay_init(void) {
+    CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+    DWT->CYCCNT = 0;
+    DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+}
+
+void delay_us(uint32_t us) {
+    uint32_t start = DWT->CYCCNT;
+    uint32_t ticks = us * (SystemCoreClock / 1000000);
+    while ((DWT->CYCCNT - start) < ticks);
+}
